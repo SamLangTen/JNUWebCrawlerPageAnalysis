c9b37c7e0ce5fbc7274fa162d572b28881337644
SamLangTen <samlangten@outlook.com>
2018-12-18 21:18:41 +0800

    修复页面，提供非域名抓取

--- WebCrawlerPageAnalysis/Program.cs
+++ WebCrawlerPageAnalysis/Program.cs
@@ -10,6 +10,27 @@ namespace WebCrawlerPageAnalysis
     {
         static void Main(string[] args)
         {
+            Console.Write("Web Link:");
+            var link = Console.ReadLine();
+            Console.Write("Visit Count:");
+            var count = int.Parse(Console.ReadLine());
+            //Do
+            var wb = new WebCrawler(link, count, true);
+
+            wb.NewPageGet += Wb_NewPageGet;
+            GetResult(wb);
+            Console.ReadLine();
+        }
+
+        private static void GetResult(WebCrawler wb)
+        {
+            var result = wb.GetPagesAsync();
+            Console.WriteLine("Total Pages:{0}", result.Count());
+        }
+
+        private static void Wb_NewPageGet(object sender, EventArgs.PageGetEventArgs e)
+        {
+            Console.WriteLine("Get:{0}\tLink:{1}", e.PageInfo.PageLink.GetLeftPart(UriPartial.Path), e.PageInfo.PageToOtherLinks.Count());
         }
     }
 }
--- WebCrawlerPageAnalysis/WebCrawler.cs
+++ WebCrawlerPageAnalysis/WebCrawler.cs
@@ -16,29 +16,60 @@ namespace WebCrawlerPageAnalysis
         private int visitPageCount { get; set; }
         private bool isRunning { get; set; } = false;
         private Uri firstPage { get; set; }
+        private bool sameHost { get; set; } = true;
         private IList<PageInfo> availablePages { get; set; } = new List<PageInfo>();
 
-        private async Task recursePage(Uri pageLink)
+        private string downloadPage(Uri link)
+        {
+            var http = (HttpWebRequest)HttpWebRequest.Create(link);
+            http.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
+            http.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+            http.AutomaticDecompression = DecompressionMethods.GZip;
+            http.Method = "GET";
+            WebResponse response;
+            string text = "";
+            try
+            {
+                response = http.GetResponse();
+                text = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                http.Abort();
+            }
+            catch (Exception)
+            {
+                http.Abort();
+                text = "";
+            }
+            return text;
+        }
+
+        private void recursePage(Uri pageLink)
         {
             //检查页面个数是否已经到达
             if (availablePages.Count >= visitPageCount || isRunning == false)
                 return;
             //开始访问
             var regex = new Regex(@"(?<=href\=\"")[^\""]*(?=\"")");
-            var client = new HttpClient();
-            var content = await client.GetStringAsync(pageLink);
+
+            Console.WriteLine("DO");
+            var content = downloadPage(pageLink);
             //排除CSS、JS脚本
             var matchURL = regex.Matches(content).Cast<Match>().Select(a => a.Value);
             //排除空URL
             matchURL = matchURL.Where(a => a.Trim() != "");
             //排除JS脚本
-            matchURL = matchURL.Where(a => !Uri.IsWellFormedUriString(a, UriKind.RelativeOrAbsolute));
-            //排除CSS、JS
-            var invalidExtensions = new string[] { "css", "js", "png", "jpg", "ico", "svg", "gif" };
-            matchURL = matchURL.Where(a => !invalidExtensions.Contains(Path.GetExtension(a).ToLower()));
+            //matchURL = matchURL.Where(a => Uri.IsWellFormedUriString(a, UriKind.RelativeOrAbsolute));
+            matchURL = matchURL.Where(a => !a.Contains("javascript"));
             //转换为URI，链接相对地址
-            var baseUri = new Uri(pageLink.Host);
+            var baseUri = new Uri(pageLink.GetLeftPart(UriPartial.Authority));
             var availableURI = matchURL.Select(a => new Uri(baseUri, a));
+            //排除CSS、JS
+            var invalidExtensions = new string[] { ".css", ".js", ".png", ".jpg", ".ico", ".svg", ".gif" };
+            availableURI = availableURI.Where(u => !invalidExtensions.Contains(Path.GetExtension(u.GetLeftPart(UriPartial.Path)).ToLower()));
+            //剔除非https和http的地址
+            availableURI = availableURI.Where(u => u.Scheme == Uri.UriSchemeHttp || u.Scheme == Uri.UriSchemeHttps);
+            //按设定选择剔除与根网站相同域名的页面
+            var authority = firstPage.GetLeftPart(UriPartial.Authority).ToLower();
+            availableURI = availableURI.Where(u => u.GetLeftPart(UriPartial.Authority).ToLower() == authority);
             //将本页添加到访问集合中
             var pi = new PageInfo()
             {
@@ -49,22 +80,22 @@ namespace WebCrawlerPageAnalysis
             //触发新页面获取事件
             this.NewPageGet?.Invoke(this, new PageGetEventArgs() { PageInfo = pi });
             //检查是否已经访问，没有就递归访问。
-            availableURI.ToList().ForEach(async u =>
+            availableURI.ToList().ForEach(u =>
             {
-                if (availablePages.FirstOrDefault(a => a.PageLink.Equals(u)) != null)
+                if (availablePages.FirstOrDefault(a => a.PageLink.GetLeftPart(UriPartial.Path).ToLower() == u.GetLeftPart(UriPartial.Path).ToLower()) == null)
                 {
-                    await recursePage(u);
+                    recursePage(u);
                 }
             });
         }
 
         public event EventHandler<PageGetEventArgs> NewPageGet;
 
-        public async Task<IList<PageInfo>> GetPagesAsync()
+        public IList<PageInfo> GetPagesAsync()
         {
             isRunning = true;
             this.availablePages.Clear();
-            await recursePage(firstPage);
+            recursePage(firstPage);
             return this.availablePages;
         }
 
@@ -72,10 +103,11 @@ namespace WebCrawlerPageAnalysis
         {
             isRunning = false;
         }
-        public WebCrawler(string firstPage,int pageCount)
+        public WebCrawler(string firstPage, int pageCount, bool isSameHost)
         {
             this.firstPage = new Uri(firstPage);
             this.visitPageCount = pageCount;
+            this.sameHost = isSameHost;
         }
     }
 }
